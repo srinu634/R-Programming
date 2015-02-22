77eadd914f6d3d91d1f7136c11a488214702b2c6
srinu634 <srinivas.suri46@gmail.com>
2015-02-22 22:21:05 +0000

    pc1 to pc5:working BN+TAN

--- pc4.R
+++ pc4.R
@@ -8,19 +8,22 @@ library(pROC)
 
 pc4 = read.arff("C:\\Users\\redhawk\\Desktop\\Thesis\\datasets\\pc4.arff") #Load the dataset
 
+excludevars <-  names(pc4) %in% c("ESSENTIAL_DENSITY")
+pc4 <- pc4[ !excludevars  ]
+
 
 pc4.test = pc4[600:705,] # Build a Test set
 #pc4 = pc4[1:599,]
-
 # str(pc4) 
 
 # Change the Strings Y,N to 1,0
-pc4$Defective <- as.numeric(factor(pc4$Defective , levels=c("N" ,"Y") ) ) 
-pc4.test$Defective <- as.numeric(pc4.test$Defective , levels=c("N" ,"Y") ) 
+pc4$c <- as.numeric(factor(pc4$c , levels=c("FALSE" ,"TRUE") ) ) 
+pc4.test$c <- as.numeric(pc4.test$c , levels=c("FALSE" ,"TRUE") ) 
 
 
 #discretize the data for bayesian networks
-pc4.disc = chiM(pc4, alpha = 0.05) 
+pc4.disc = chiM(pc4, alpha = 0.05)
+pc4.disc <- pc4.disc[c(1:14,16:38)]
 pc4.test.disc = chiM(pc4.test,alpha=0.05)
 
 #load the data into a data frame
@@ -33,30 +36,32 @@ pc4.disc.data[,names(pc4)] <- lapply(pc4.disc.data[,names(pc4)] , factor)
 pc4.test.data[,names(pc4.test)] <- lapply(pc4.test.data[,names(pc4.test)] , factor) 
 #str(pc4.test.set.data)
 
+pc4.disc.data <- pc4.disc.data[,"ESSENTIAL_DENSITY"] 
+
 
 #Building a Naive Bayes classifier
-pc4.bn = naive.bayes(pc4.disc.data, "Defective")
+pc4.bn = naive.bayes(pc4.disc.data, "c")
 pc4.pred.bn = predict(pc4.bn, pc4.disc.data)  #2nd parameter should be pc4.test.data
-table(pc4.pred.bn,pc4.disc.data[,"Defective"])   #output the prediction matrix
+table(pc4.pred.bn,pc4.disc.data[,"c"])   #output the prediction matrix
 
 #Change the outputs to numeric values; Happens at two levels. 1) Change the params to characters 2) Change the characters to numeric.
 pc4.given.bn <- as.numeric(as.character(pc4.pred.bn))
-pc4.predicted.bn <- as.numeric(as.character(pc4.disc.data[,"Defective"]))
+pc4.predicted.bn <- as.numeric(as.character(pc4.disc.data[,"c"]))
 accuracy(f = pc4.given.bn , x = pc4.predicted.bn)  #print the accuracy
 
 
 #Building a Tree Augmented Network classifier
-pc4.tan = tree.bayes(pc4.disc.data, "Defective")
+pc4.tan = tree.bayes(pc4.disc.data, "c")
 #graphviz.plot(pc4.tan)
 pc4.fitted = bn.fit(pc4.tan, pc4.disc.data)
 #coefficients(pc4.fitted)
-pc4.pred.tan <- predict(pc4.fitted$Defective, pc4.disc.data) #2nd parameter should be pc4.test.data
+pc4.pred.tan <- predict(pc4.fitted$c, pc4.disc.data) #2nd parameter should be pc4.test.data
 #pc4.pred.tan <- as.numeric(pc4.pred.tan)
 #pc4.perf.tan <- performance(pc4.pred.tan,  measure = "tpr",x.measure =   "fpr")
-table(pc4.pred.tan, pc4.disc.data[, "Defective"]) #output the prediction matrix
+table(pc4.pred.tan, pc4.disc.data[, "c"]) #output the prediction matrix
 #Change the outputs to numeric values; 
 pc4.given.tan <- as.numeric(as.character(pc4.pred.tan))
-pc4.pred.tan <- as.numeric(as.character(pc4.disc.data[,"Defective"]))
+pc4.pred.tan <- as.numeric(as.character(pc4.disc.data[,"c"]))
 accuracy(f = pc4.given.tan , x = pc4.pred.tan)  #print the accuracy
 
 #score(pc4.tan, pc4.disc.data, type = "bde")
--- pc5.R
+++ pc5.R
@@ -6,58 +6,61 @@ library(ROCR)
 library(pROC)
 
 
-pc3 = read.arff("C:\\Users\\redhawk\\Desktop\\Thesis\\datasets\\pc3.arff") #Load the dataset
+pc5 = read.arff("C:\\Users\\redhawk\\Desktop\\Thesis\\datasets\\pc5.arff") #Load the dataset
+
+excludevars <-  names(pc5) %in% c("PARAMETER_COUNT")
+pc5 <- pc5[ !excludevars  ]
+
+pc5.test = pc5[600:705,] # Build a Test set
 
 
-pc3.test = pc3[600:705,] # Build a Test set
-#pc3 = pc3[1:599,]
-
-# str(pc3) 
-
+# str(pc5) 
+#options(error=recover)
+#reach_full_in <- reachability(krack_full, 'in')
 # Change the Strings Y,N to 1,0
-pc3$Defective <- as.numeric(factor(pc3$Defective , levels=c("N" ,"Y") ) ) 
-pc3.test$Defective <- as.numeric(pc3.test$Defective , levels=c("N" ,"Y") ) 
+pc5$c <- as.numeric(factor(pc5$c , levels=c("FALSE" ,"TRUE") ) ) 
+pc5.test$c <- as.numeric(pc5.test$c , levels=c("FALSE" ,"TRUE") ) 
 
-
+#traceback()
 #discretize the data for bayesian networks
-pc3.disc = chiM(pc3, alpha = 0.05) 
-pc3.test.disc = chiM(pc3.test,alpha=0.05)
+pc5.disc = chiM(pc5, alpha = 0.05) 
+pc5.test.disc = chiM(pc5.test,alpha=0.05)
 
 #load the data into a data frame
-pc3.disc.data = data.frame(pc3.disc$Disc.data)
-pc3.test.data = data.frame(pc3.test.disc$Disc.data)
+pc5.disc.data = data.frame(pc5.disc$Disc.data)
+pc5.test.data = data.frame(pc5.test.disc$Disc.data)
 
 
 #Change all the discretized values into factors
-pc3.disc.data[,names(pc3)] <- lapply(pc3.disc.data[,names(pc3)] , factor) 
-pc3.test.data[,names(pc3.test)] <- lapply(pc3.test.data[,names(pc3.test)] , factor) 
-#str(pc3.test.set.data)
+pc5.disc.data[,names(pc5)] <- lapply(pc5.disc.data[,names(pc5)] , factor) 
+pc5.test.data[,names(pc5.test)] <- lapply(pc5.test.data[,names(pc5.test)] , factor) 
+#str(pc5.test.set.data)
 
 
 #Building a Naive Bayes classifier
-pc3.bn = naive.bayes(pc3.disc.data, "Defective")
-pc3.pred.bn = predict(pc3.bn, pc3.disc.data)  #2nd parameter should be pc3.test.data
-table(pc3.pred.bn,pc3.disc.data[,"Defective"])   #output the prediction matrix
+pc5.bn = naive.bayes(pc5.disc.data, "c")
+pc5.pred.bn = predict(pc5.bn, pc5.disc.data)  #2nd parameter should be pc5.test.data
+table(pc5.pred.bn,pc5.disc.data[,"c"])   #output the prediction matrix
 
 #Change the outputs to numeric values; Happens at two levels. 1) Change the params to characters 2) Change the characters to numeric.
-pc3.given.bn <- as.numeric(as.character(pc3.pred.bn))
-pc3.predicted.bn <- as.numeric(as.character(pc3.disc.data[,"Defective"]))
-accuracy(f = pc3.given.bn , x = pc3.predicted.bn)  #print the accuracy
+pc5.given.bn <- as.numeric(as.character(pc5.pred.bn))
+pc5.predicted.bn <- as.numeric(as.character(pc5.disc.data[,"c"]))
+accuracy(f = pc5.given.bn , x = pc5.predicted.bn)  #print the accuracy
 
 
 #Building a Tree Augmented Network classifier
-pc3.tan = tree.bayes(pc3.disc.data, "Defective")
-#graphviz.plot(pc3.tan)
-pc3.fitted = bn.fit(pc3.tan, pc3.disc.data)
-#coefficients(pc3.fitted)
-pc3.pred.tan <- predict(pc3.fitted$Defective, pc3.disc.data) #2nd parameter should be pc3.test.data
-#pc3.pred.tan <- as.numeric(pc3.pred.tan)
-#pc3.perf.tan <- performance(pc3.pred.tan,  measure = "tpr",x.measure =   "fpr")
-table(pc3.pred.tan, pc3.disc.data[, "Defective"]) #output the prediction matrix
+pc5.tan = tree.bayes(pc5.disc.data, "c")
+#graphviz.plot(pc5.tan)
+pc5.fitted = bn.fit(pc5.tan, pc5.disc.data)
+#coefficients(pc5.fitted)
+pc5.pred.tan <- predict(pc5.fitted$c, pc5.disc.data) #2nd parameter should be pc5.test.data
+#pc5.pred.tan <- as.numeric(pc5.pred.tan)
+#pc5.perf.tan <- performance(pc5.pred.tan,  measure = "tpr",x.measure =   "fpr")
+table(pc5.pred.tan, pc5.disc.data[, "c"]) #output the prediction matrix
 #Change the outputs to numeric values; 
-pc3.given.tan <- as.numeric(as.character(pc3.pred.tan))
-pc3.pred.tan <- as.numeric(as.character(pc3.disc.data[,"Defective"]))
-accuracy(f = pc3.given.tan , x = pc3.pred.tan)  #print the accuracy
+pc5.given.tan <- as.numeric(as.character(pc5.pred.tan))
+pc5.pred.tan <- as.numeric(as.character(pc5.disc.data[,"c"]))
+accuracy(f = pc5.given.tan , x = pc5.pred.tan)  #print the accuracy
 
-#score(pc3.tan, pc3.disc.data, type = "bde")
-#score(pc3.bn,pc3.disc.data,type="bde")
+#score(pc5.tan, pc5.disc.data, type = "bde")
+#score(pc5.bn,pc5.disc.data,type="bde")
