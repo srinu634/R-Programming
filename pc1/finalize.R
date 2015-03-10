f3add0c09cd6675d846a86ced950a6d31562381c
srinu634 <srinivas.suri46@gmail.com>
2015-03-10 22:25:28 +0000

    checkpoint : 3

--- pc1/finalize.R
+++ pc1/finalize.R
@@ -1,59 +1,10 @@
-library(infotheo)
-library(foreign)
-library(bnlearn)
-library(discretization)
-library(ROCR)
-library(pROC)
-library(forecast)
-library(base)
-library(gRain)
-library(caTools)
-
-setwd("C:\\Users\\redhawk\\Documents\\GitHub\\R-Programming\\pc1")
-
-#sink("pc1.log.txt")
-
-source("initialize.R")
-source("start.R")
-source("preprocess.R")
-source("drawPlot.R")
-source("bn.r")
-source("TAN.R")
-source("hc.R")
-source("tabu.R")
-source("mmhc.R")
-source("HITON.R")
-source("RSMAX2.R")
-source("mmpc.R")
-source("gs.R")
-source("mmpc.R")
-source("IAMB.R")
-#source("fastIAMB.R")
-#source("interIAMB.R")
-
-
-initialize() ;
-
-
-debug = FALSE
-
- i= 0
-  for( i in 1:10){
-   paste("In iteration",i ,sep= " ")
-    startProcess(debug,i)
-  }
-
-  finalize() #Wrap up things. Get the average AUC measures.
-  setwd("C:\\Users\\redhawk\\Documents\\GitHub\\R-Programming\\pc1")
-  
-###############################################################################
 #
 auc.bn.avg <- NULL
 
 for( i in 1: ( length(pc1.disc.data) -1 ) ) {
   #print(  str(auc.bn)[i]  )
   #print ( mean( auc.bn[, i ] ) )
-  
+
   auc.bn.avg <- c( auc.bn.avg, mean(auc.bn[,i])) 
 }
 
@@ -203,6 +154,7 @@ for( i in 1: ( length(pc1.disc.data) -1 ) ) {
 }
 
 auc.rsmax2.avg
+
 
 
 
